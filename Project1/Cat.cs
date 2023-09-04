cee6bc9c2463a6e1f066b6487c684bb56c632bb5
Yusuf Praditya <47532266+yusufpraditya@users.noreply.github.com>
2023-09-04 11:54:53 +0700

    buat class Cat

--- Project1/Cat.cs
+++ Project1/Cat.cs
@@ -2,4 +2,23 @@ namespace Project1;
 
 public class Cat
 {
+	public string? colour;
+	public string? race;
+	public int? age;
+	
+	public void Meow() 
+	{
+		Console.WriteLine("Meow");
+	}
+	public void Eat() 
+	{
+		Console.WriteLine("Eat");
+	}
+	public void Walk() 
+	{
+		Console.WriteLine("Walk");
+	}
+	public void Sleep() { 
+		Console.WriteLine("Sleep");
+	}
 }
--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,7 +1,14 @@
-﻿class Program 
+﻿using Project1;
+class Program 
 {
 	static void Main() 
-    {
-    	Console.WriteLine("test");
-    }
+	{
+		Console.WriteLine("Keseharian kucing");
+		
+		Cat kucing1 = new Cat();
+		kucing1.Eat();
+		kucing1.Meow();
+		kucing1.Walk();
+		kucing1.Sleep();
+	}
 }
