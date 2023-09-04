7fd14dcda47df882d61bf0fb303d6735e48ead10
Yusuf Praditya <47532266+yusufpraditya@users.noreply.github.com>
2023-09-04 11:59:30 +0700

    Create Dog.cs

---
+++ Project1/Dog.cs
@@ -0,0 +1,24 @@
+namespace Project1;
+
+public class Dog
+{
+	public string? colour;
+	public string? race;
+	public int? age;
+	
+	public void Bark() 
+	{
+		Console.WriteLine("Bark");
+	}
+	public void Eat() 
+	{
+		Console.WriteLine("Eat");
+	}
+	public void Walk() 
+	{
+		Console.WriteLine("Walk");
+	}
+	public void Sleep() { 
+		Console.WriteLine("Sleep");
+	}
+}
