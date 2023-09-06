763e63d73dd66037a7a8341bca4b72c7d4894e0c
Yusuf Praditya <47532266+yusufpraditya@users.noreply.github.com>
2023-09-07 06:25:09 +0700

    Update Cat.cs

--- W1 D3/MethodHiding/Cat.cs
+++ W1 D3/MethodHiding/Cat.cs
@@ -4,6 +4,6 @@ public class Cat : Animal
 {
 	public new void Move() //  It is recommended to use new keyword to hide method
 	{
-		Console.WriteLine("Cat is moving..");
+		Console.WriteLine("Cat is walking..");
 	}
 }
