fce4c536258ed53125ad37a9fa1e66f0d2f32174
Yusuf Praditya <47532266+yusufpraditya@users.noreply.github.com>
2023-09-10 21:49:48 +0700

    Add ExtensionMethod project

---
+++ W1 D3/ExtensionMethod/MyExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace ExtensionMethod;
+
+public static class MyExtension
+{
+	public static string RemoveVowels(this string text) 
+	{
+		StringBuilder NewString = new();
+		text = text.ToLower();
+		for (int i = 0; i < text.Length; i++) 
+		{
+			if (text[i] != 'a' && text[i] != 'i' && text[i] != 'u' && text[i] != 'e' && text[i] != 'o') 
+			{
+				NewString.Append(text[i]);
+			}
+		}
+		return NewString.ToString();
+	} 
+}
---
+++ W1 D3/ExtensionMethod/Program.cs
@@ -0,0 +1,14 @@
+﻿using ExtensionMethod;
+
+public class Program 
+{
+	static void Main() 
+	{
+		string Text = "halo namaku yusuf";
+		string NewText = Text.RemoveVowels();
+		Console.WriteLine(Text);
+		Console.WriteLine(NewText);
+		
+		Console.WriteLine(MyExtension.RemoveVowels("saat ini aku sedang ikut bootcamp di formulatrix."));
+	}
+}
