0bbe3cee3d856f3fabce3340f80863e64e6e900b
Yusuf Praditya <47532266+yusufpraditya@users.noreply.github.com>
2023-10-04 12:04:13 +0700

    Add W5 D3 programs

---
+++ W5 D3/Task/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Program 
+{
+	static void Main() 
+	{
+		// Task runs on background thread
+		Task task1 = new Task(Print);
+		task1.Start();
+		task1.Wait();
+		
+		Task.Run(() => 
+		{
+			Console.WriteLine("Halo dunia");
+		});
+		
+		Task task2 = Task.Run(() => 
+		{
+			Console.WriteLine("Do something");
+		});
+		
+		Task task3 = Task.Run(() => 
+		{
+			Console.WriteLine("Do another thing");
+		});
+		
+		Task.WaitAll(task2, task3);
+		
+		Console.WriteLine("Task delay started");
+		Task task4 = Task.Run(async () => 
+		{
+			Console.WriteLine("Process A");
+			await Task.Delay(1000);
+			Console.WriteLine("Process finished");
+		});
+		task4.Wait();
+		Console.WriteLine("Task delay finished");
+		
+	}
+	
+	static void Print() { 
+		Console.WriteLine("Hello world");
+	}
+}
---
+++ W5 D3/Threading/Program.cs
@@ -0,0 +1,68 @@
+﻿
+using System.Diagnostics;
+
+public class Program 
+{
+	static void Main() 
+	{
+		Thread t1 = new Thread(ProcessA);
+		Thread t2 = new Thread(ProcessB);
+		
+		// it's uncertain which thread is started first
+		t1.Start();
+		t2.Start();
+		Console.WriteLine("Finished");
+		
+		// Blocking main thread
+		Stopwatch sw = new();
+		sw.Start();
+		Thread.Sleep(1000); // 1000 miliseconds
+		sw.Stop();
+		Console.WriteLine("Main thread has been blocked in " + sw.Elapsed);
+		
+		// Lambda expression
+		Thread t3 = new Thread(() => 
+		{
+			Console.WriteLine("My thread 3");
+		});
+		Thread t4 = new Thread(() => 
+		{
+			Console.WriteLine("My thread 4");
+		});
+		
+		t3.Start();
+		t4.Start();
+		
+		Thread t5 = new Thread(PrintX);
+		Thread t6 = new Thread(PrintY);
+		
+		t5.Start();
+		t6.Start();
+	}
+
+	static void ProcessA() 
+	{
+		Console.WriteLine("Hello world");
+	}
+	
+	static void ProcessB() 
+	{
+		Console.WriteLine("Halo dunia");
+	}
+	
+	static void PrintX() 
+	{
+		for (int i = 0; i < 10; i++) 
+		{
+			Console.Write("x");
+		}
+	}
+	
+	static void PrintY() 
+	{
+		for (int i = 0; i < 10; i++) 
+		{
+			Console.Write("y");
+		}
+	}
+}
