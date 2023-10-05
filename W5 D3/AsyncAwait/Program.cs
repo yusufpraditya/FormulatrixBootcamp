15a04508df711384630250d46028a53ea203f5c5
Yusuf Praditya <47532266+yusufpraditya@users.noreply.github.com>
2023-10-05 12:18:07 +0700

    Add W5 D4 program

--- W5 D3/AsyncAwait/Program.cs
+++ W5 D3/AsyncAwait/Program.cs
@@ -1,7 +1,28 @@
-﻿public class Program 
+﻿using System.Threading.Tasks;
+
+public class Program 
 {
 	static void Main() 
-    {
-    	
-    }
+	{
+		Console.WriteLine("Hello");
+		Task.Run(LoopAsync);
+		while (true) 
+		{
+			string? input = Console.ReadLine();
+			if (input == "x") break;
+			else Console.WriteLine("Your input: " + input);
+		}
+	}
+	
+	static async Task LoopAsync() 
+	{
+		await Task.Run(async () => 
+		{
+			for (int i = 0; i < 100; i++) 
+			{
+				Console.WriteLine(i);
+				await Task.Delay(1000);
+			}
+		});
+	}
 }
---
+++ W5 D4/CancellationToken/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program 
+{
+	static void Main()
+	{
+		CancellationTokenSource cts = new();
+		CancellationToken token = cts.Token;
+		
+		Task t1 = Task.Run(() => PrintLoop(token));
+		
+		while (!t1.IsCanceled)
+		{
+			if (Console.ReadKey().KeyChar == 'x') 
+			{
+				cts.Cancel();
+				break;
+			}
+		}
+	}
+	
+	static async void PrintLoop(CancellationToken token) 
+	{
+		int counter = 0;
+		while (!token.IsCancellationRequested) 
+		{
+			Console.WriteLine(counter++);
+			await Task.Delay(1000);
+		}
+	}
+}
