afa84c3b64974f4c95e8108c745b8c7097835c56
Yusuf Praditya <47532266+yusufpraditya@users.noreply.github.com>
2023-10-06 11:13:47 +0700

    Update W5 D5 program

---
+++ W5 D5/PreprocessingDirectives/Program.cs
@@ -0,0 +1,16 @@
+﻿#define DEVELOPMENT
+public class Program 
+{
+	static void Main() 
+	{
+		#if PRODUCTION
+		Console.WriteLine("Production");
+		#error "Error"
+		#elif DEVELOPMENT
+		Console.WriteLine("Development");
+		#warning "Warning"
+		#elif DEBUG
+		Console.WriteLine("Debug");
+		#endif
+	}
+}
