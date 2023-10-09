411a35511e6284f69cc743fdfeb3123b4845175c
Yusuf Praditya <47532266+yusufpraditya@users.noreply.github.com>
2023-10-09 11:50:16 +0700

    Add W6 D1 programs

---
+++ W6 D1/FileStream/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.IO;
+
+public class Program 
+{
+	static void Main()
+	{
+		int offset = 0;
+		
+		// Write to a file
+		string myString = "Hello World";
+		using(FileStream fs = new(@"./MyString.txt", FileMode.Create, FileAccess.Write)) {
+			byte[] bytes = Encoding.UTF8.GetBytes(myString);
+			fs.Write(bytes, offset, bytes.Length - offset);
+		}
+		
+		// Read from a file
+		using(FileStream fs = new(@"./MyString2.txt", FileMode.Open, FileAccess.Read)) 
+		{
+			Console.WriteLine(fs.Length);
+			byte[] bytes = new byte[fs.Length];
+			fs.Read(bytes, offset, bytes.Length - offset);
+			string myString2 = Encoding.UTF8.GetString(bytes);
+			Console.WriteLine(myString2);
+		}
+		
+		// FileShare
+		using(FileStream fs = new(@"./MyString3.txt", FileMode.Open, FileAccess.Read, FileShare.None)) 
+		{
+			while (true) 
+			{
+				
+			}
+		}
+	}
+}
---
+++ W6 D1/StreamWriterAndStreamReader/Program.cs
@@ -0,0 +1,54 @@
+﻿public class Program 
+{
+	static void Main() 
+	{
+		string file1 = @"./Text1.txt";
+		string file2 = @"./Text2.txt";
+		string file3 = @"./Text3.txt";
+		
+		using (FileStream fs = new(file1, FileMode.OpenOrCreate, FileAccess.ReadWrite)) 
+		{
+			using (StreamWriter sw = new(fs)) 
+			{
+				sw.BaseStream.Position = fs.Seek(0, SeekOrigin.End);
+				sw.WriteLine();
+				sw.WriteLine("This is new line");
+			}
+		}
+		string[] myString = File.ReadAllLines(file1);
+		foreach (var s in myString) 
+		{
+			Console.WriteLine(s);
+		}
+		
+		// Modify specific line
+		int line = 2;
+		string newString = "This is new line";
+		string[] myString2 = File.ReadAllLines(file2);
+		using (FileStream fs = new(file2, FileMode.OpenOrCreate, FileAccess.ReadWrite)) 
+		{
+			using (StreamWriter sw = new(fs)) 
+			{
+				myString2[line - 1] = newString;
+				foreach (var s in myString2) 
+				{
+					sw.WriteLine(s);
+				}
+			}
+		}
+		
+		// Async method
+		Task.Run(async () => 
+		{
+			using (FileStream fs = new(file3, FileMode.OpenOrCreate, FileAccess.ReadWrite)) 
+			{
+				using (StreamWriter sw = new(fs)) 
+				{
+					sw.BaseStream.Position = fs.Seek(0, SeekOrigin.End);
+					await sw.WriteLineAsync("Hello");
+					await sw.WriteLineAsync("Hi");
+				}
+			}
+		}).Wait();
+	}
+}
